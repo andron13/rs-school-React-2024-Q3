39d4402307524338283a8c960790c9df05a868e8
andron13 <andron13@gmail.com>
2024-07-04 16:47:30 +0200

    feat: add spinner

--- src/widgets/MainSection/MainSection.tsx
+++ src/widgets/MainSection/MainSection.tsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 
+import imgSrc from "§/assets/images/spinner.png";
 import NotFoundPage from "§features/NotFoundPage";
 import fetchCharacters, { ApiResponse, Character } from "§shared/api/api.ts";
 import CharacterList from "§widgets/CharacterList";
@@ -11,6 +12,7 @@ interface MainSectionProps {
 interface MainSectionState {
   data: Character[] | null;
   error: string | null;
+  loading: boolean;
 }
 
 class MainSection extends Component<MainSectionProps, MainSectionState> {
@@ -19,22 +21,28 @@ class MainSection extends Component<MainSectionProps, MainSectionState> {
     this.state = {
       data: null,
       error: null,
+      loading: false,
     };
   }
 
   fetchDataFromServer = async (result: string) => {
+    this.setState({ loading: true });
     try {
       const response: ApiResponse | number = await fetchCharacters(result);
       if (typeof response === "number") {
         throw new Error(`Failed to fetch. Status code: ${response}`);
       }
-      this.setState({
-        data: response.results,
-        error: null,
-      });
+      setTimeout(() => {
+        this.setState({
+          data: response.results,
+          error: null,
+          loading: false,
+        });
+      }, 500);
     } catch (error) {
       this.setState({
         error: error.message,
+        loading: false,
       });
     }
   };
@@ -45,25 +53,38 @@ class MainSection extends Component<MainSectionProps, MainSectionState> {
 
   componentDidUpdate(prevProps: MainSectionProps) {
     if (prevProps.searchString !== this.props.searchString) {
+      this.setState({ loading: true });
       this.fetchDataFromServer(this.props.searchString).then(() => {});
     }
   }
 
   render() {
     const { searchString } = this.props;
-    const { data, error } = this.state;
+    const { data, error, loading } = this.state;
 
     return (
       <main className="bg-gray-200 flex-1 flex flex-col items-center justify-center">
         <div className="max-w-screen-lg mx-auto p-4">
-          <p className="mb-4">
-            Search-string:{" "}
-            <span className="font-bold">{searchString || "empty"}</span>
-          </p>
           {error ? (
             <NotFoundPage />
           ) : (
-            <>{data && <CharacterList characters={data} />}</>
+            <>
+              {loading ? (
+                <img
+                  src={imgSrc}
+                  alt="spinner"
+                  className="w-32 h-32 animate-spin"
+                />
+              ) : (
+                <>
+                  <p className="mb-4">
+                    Search-string:{" "}
+                    <span className="font-bold">{searchString || "empty"}</span>
+                  </p>
+                  {data && <CharacterList characters={data} />}
+                </>
+              )}
+            </>
           )}
         </div>
       </main>
